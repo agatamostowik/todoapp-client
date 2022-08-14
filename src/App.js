e2b8618c1b78fa72c8d24b87a3a9b836a6cda696
Agata Mostowik <agatamostowik@gmail.com>
2022-08-14 16:03:39 +0200

    Rendered list of nested todos based on mock data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { TodoList } from "./components/TodoList";
+
+// const todosFlat = [
+//   {
+//     _id: "1",
+//     label: "A",
+//     todoIds: ["2", "3"],
+//   },
+//   {
+//     _id: "4",
+//     label: "B",
+//     todoIds: [],
+//   },
+//   {
+//     _id: "2",
+//     label: "C",
+//     todoIds: [],
+//   },
+//   {
+//     _id: "3",
+//     label: "D",
+//     todoIds: ["5", "6"],
+//   },
+//   {
+//     _id: "5",
+//     label: "H",
+//     todoIds: [],
+//   },
+//   {
+//     _id: "6",
+//     label: "L",
+//     todoIds: [],
+//   },
+//   {
+//     _id: "7",
+//     label: "P",
+//     todoIds: [],
+//   },
+// ];
+
+const todosNested = [
+  {
+    _id: "1",
+    label: "A",
+    todoIds: [
+      {
+        _id: "2",
+        label: "C",
+        todoIds: [],
+      },
+      {
+        _id: "3",
+        label: "D",
+        todoIds: [
+          {
+            _id: "5",
+            label: "H",
+            todoIds: [],
+          },
+          {
+            _id: "6",
+            label: "L",
+            todoIds: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    _id: "4",
+    label: "B",
+    todoIds: [],
+  },
+  {
+    _id: "7",
+    label: "P",
+    todoIds: [],
+  },
+];
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+
+  return <TodoList isRoot={true} todos={todosNested} />;
+
 }
 
 export default App;
